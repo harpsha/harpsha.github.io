b7f21fb96be5beaceb835509744345ccb05f19d1
Harpreet Singh <harpreet.iitk@gmail.com>
2016-02-13 11:25:50 +0530

    initial9

--- components/Carousel.js
+++ components/Carousel.js
@@ -1,6 +1,5 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var Carousel = require('nuka-carousel');
 
 var baseUrl = '';
 if (process.env.NODE_ENV === 'production') {
@@ -8,17 +7,68 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 const CarouselExp = React.createClass({
-  mixins: [Carousel.ControllerMixin],
   render() {
     return (
-    	<div style={{width: '70%', margin: 'auto'}}>
- 			<Carousel dragging={true}>
-        		<img src={baseUrl + '/img/appeti_home0.jpg'}/>
-        		<img src={baseUrl + '/img/appeti_home1.jpg'}/>
-        		<img src={baseUrl + '/img/appeti_home2.jpg'}/>
-            <img src={baseUrl + '/img/appeti_home3.jpg'}/>
-      		</Carousel>
-      	 </div>
+      <div id="example1" className="slider-pro">
+        <div className="sp-slides">
+          <div className="sp-slide">
+            <img className="sp-image" src="img/blank.gif"
+              data-src="img/appeti_home0.jpg"
+              data-retina="img/appeti_home0.jpg"/>
+          </div>
+
+              <div className="sp-slide">
+                <img className="sp-image" src="img/blank.gif"
+                  data-src="img/appeti_home1.jpg"
+                  data-retina="img/appeti_home1.jpg"/>
+          </div>
+
+          <div className="sp-slide">
+            <img className="sp-image" src="img/blank.gif"
+              data-src="img/appeti_home2.jpg"
+              data-retina="img/appeti_home2.jpg"/>
+          </div>
+
+          <div className="sp-slide">
+            <img className="sp-image" src="img/blank.gif"
+              data-src="img/appeti_home3.jpg"
+              data-retina="img/appeti_home3.jpg"/>
+          </div>
+
+          <div className="sp-slide">
+            <img className="sp-image" src="img/blank.gif"
+              data-src="img/appeti_home4.jpg"
+              data-retina="img/appeti_home4.jpg"/>
+          </div>
+        </div>
+
+        <div className="sp-thumbnails">
+          <div className="sp-thumbnail">
+            <div className="sp-thumbnail-title">Appeti!</div>
+            <div className="sp-thumbnail-description">How it works</div>
+          </div>
+
+          <div className="sp-thumbnail">
+            <div className="sp-thumbnail-title">Republic Day Sale!</div>
+            <div className="sp-thumbnail-description">Flat 13% Off Site-wide</div>
+          </div>
+
+          <div className="sp-thumbnail">
+            <div className="sp-thumbnail-title">Chocolate fever!</div>
+            <div className="sp-thumbnail-description">Half kg chocolates for Rs499</div>
+          </div>
+
+          <div className="sp-thumbnail">
+            <div className="sp-thumbnail-title">Khakhra Combos</div>
+            <div className="sp-thumbnail-description">In 5 different flavours</div>
+          </div>
+
+          <div className="sp-thumbnail">
+            <div className="sp-thumbnail-title">Other delicacies</div>
+            <div className="sp-thumbnail-description">Cakes, cookies and more</div>
+          </div>
+        </div>
+        </div>
     )
   }
 });
